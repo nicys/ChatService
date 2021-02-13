1e510e2b97c6a3d81196a026393181f13c4c63ab
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-13 12:18:22 +0700

    object ChatService added.

---
+++ src/main/kotlin/ChatService.kt
@@ -0,0 +1,10 @@
+object ChatService {
+    var chats = mutableListOf<Chat>()
+    var users = mutableListOf<User>()
+    var messages = mutableListOf<Message>()
+
+
+
+}
+
+    fun isUser(userId: Int) = users.filter {}
