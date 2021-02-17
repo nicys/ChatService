7e73ba7987260b7c260c1cf060548a8a8c9ad44e
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-17 22:22:20 +0700

    Chat.kt corrected.

--- src/main/kotlin/Chat.kt
+++ src/main/kotlin/Chat.kt
@@ -1,15 +1,30 @@
 data class Chat(
-    val idChat: Int,
-    val idOwn: Int
+        val idChat: Int,
+        val nameOwn: String = "Nik",
+        val userId: Int,
+        var isDeletedUser: Boolean = false,
+        var isDeleted: Boolean = false
 )
 
 data class User(
-    val idUser: Int,
-    val name: String,
-    val sirName: String
-)
+    val idUser: Int = 0,
+    val name: String = "noName",
+    val sirName: String = "noSirName",
+    val isDeleted: Boolean = false
+) {
+    override fun toString(): String {
+        return """|  
+            |  User: idUser = $idUser, 
+            |name = '$name', sirName = '$sirName', 
+            |deleted = $isDeleted""".trimMargin()
+    }
+}
 
 data class Message(
-    val idMessage: Int,
-    val text: String
+        val chatId: Int,
+        val userId: Int,
+        val idMessage: Int,
+        val text: String?,
+        var isRead: Boolean = false,
+        var isDeletedMessage: Boolean = false
 )
