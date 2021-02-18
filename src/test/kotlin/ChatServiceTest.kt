e051da66fd51fb4ad4ed344afed532bdd1df516d
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-18 16:57:51 +0700

    ChatServiceTest.kt added.

---
+++ src/test/kotlin/ChatServiceTest.kt
@@ -0,0 +1,42 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ChatServiceTest {
+
+    @Test
+    fun addUser() {
+    }
+
+    @Test
+    fun deleteUser() {
+    }
+
+    @Test
+    fun createChatFirstMessage() {
+    }
+
+    @Test
+    fun deleteChat() {
+    }
+
+    @Test
+    fun createNewMessage() {
+    }
+
+    @Test
+    fun deleteMessage() {
+    }
+
+    @Test
+    fun editMessage() {
+    }
+
+    @Test
+    fun receiveUnreadMessagesInChat() {
+    }
+
+    @Test
+    fun receiveAllMessagesInChat() {
+    }
+}
