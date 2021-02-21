95dec4c9f7fafd3281cac1b7fefe2056788cb3dd
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-22 01:05:49 +0700

    ChatServiceTest.kt was corrected.

--- src/main/kotlin/ChatInterface.kt
+++ src/main/kotlin/ChatInterface.kt
@@ -4,15 +4,15 @@ interface ChatInterface<C, M, U> {
 
     fun deleteUser(u: U, i: Int): Boolean
 
-    fun createChatFirstMessage(m: M, c: C, i: Int): M
+    fun addChat(c: C, i: Int): Boolean
 
     fun deleteChat(c: C, i: Int, y: Int): Boolean
 
-    fun createNewMessage(m: M, i: Int, y: Int): M
+    fun createMessage(m: M, i: Int, y: Int): M
 
     fun deleteMessage(c: C, m: M, i: Int, y: Int, z: Int): Boolean
 
-    fun editMessage(m: M, i: Int, y: Int, z: Int): Boolean
+    fun editMessage(m: M, i: Int): Boolean
 
     fun receiveUnreadMessagesInChat()
 
